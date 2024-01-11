8d6cecbc6e4146e7f38b7af811f0a4eb37c1ef54
Nicolas Allesøe <58513123+NicoCPH@users.noreply.github.com>
2024-01-11 17:40:53 +0100

    Update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,12 +1,17 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  ignoredRouteFiles: ["**/.*"],
-  routes(defineRoutes) {
-    return defineRoutes((route) => {
+exports.routes = async (defineRoutes) => {
+  // If you need to do async work, do it before calling `defineRoutes`, we use
+  // the call stack of `route` inside to set nesting.
+
+  return defineRoutes((route) => {
     route("/", "routes/_index.tsx", { index: true });
     route("about", "routes/about.tsx");
-    });
-    },
+  });
+};
+
+export default {
+  ignoredRouteFiles: ["**/.*"],
+    
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
