dc9b434052368d09ef0e0b0eb29fc3333c5e7ecd
Nicolas Allesøe <58513123+NicoCPH@users.noreply.github.com>
2024-01-12 18:30:48 +0100

    comeon

--- remix.config.js
+++ remix.config.js
@@ -5,8 +5,8 @@ export default {
   ignoredRouteFiles: ["**/.*"],
   routes(defineRoutes) {
     return defineRoutes((route) => {
-    route("/", path.join("extra", "_index.tsx"));
-    route("/about", path.join("extra", "about.tsx"));
+    route("/", path.join("routes", "_index.tsx"));
+    route("/about", path.join("routes", "about.tsx"));
     });
     },
     // serverModuleFormat: "cjs",
